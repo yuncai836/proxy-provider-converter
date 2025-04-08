e126f590a2341b637d1afc34112fd4afefd4afbf
yuncai836 <167850518+yuncai836@users.noreply.github.com>
2025-04-08 13:31:33 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import splitbee from "@splitbee/web";
-
+export const runtime = "edge";
 function MyApp({ Component, pageProps }) {
   splitbee.init({
     scriptUrl: "/bee.js",
