f5d5c11076560722c3b8c51011525f44e11aad1b
yuncai836 <167850518+yuncai836@users.noreply.github.com>
2025-04-08 13:23:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ let host = "";
 if (typeof window !== "undefined") {
   host = window.location.origin;
 }
-
+export const runtime = "edge";
 export default function Home() {
   const [url, setUrl] = useState("");
   const [target, setTarget] = useState("clash");
